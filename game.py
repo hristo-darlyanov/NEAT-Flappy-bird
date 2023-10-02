99859af47e3aa0584b96cdba360711a5750b1fe1
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-10-02 16:41:01 +0300

    Adding bird class and initializing bird in pygame

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
+from math import fabs
+from cv2 import rotate
+from networkx import center
 import pygame
 import neat
 import time
 import os
 import random
 
-WIN_WIDTH = 600
-WIN_HEIGHT = 800
+WIN_WIDTH = 500
+WIN_HEIGHT = 750
 
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))), pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))), pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
@@ -21,7 +24,7 @@ class Bird():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.tile = 0
+        self.tilt = 0
         self.tick_count = 0
         self.vel = 0
         self.height = self.y
@@ -33,4 +36,72 @@ class Bird():
         self.tick_count = 0
         self.height = self.y
 
+    def move(self):
+        self.tick_count += 1
         
+        d = self.vel * self.tick_count + 1.5*self.tick_count**2
+        if d >= 16:
+            d = 16
+        
+        if d < 0:
+            d -=2
+            
+        self.y = self.y + d
+        
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+
+    def draw(self, win):
+        self.image_count += 1
+
+        if self.image_count < self.ANIMATION_TIME:
+            self.image = self.IMGS[0]
+        elif self.image_count < self.ANIMATION_TIME * 2:
+            self.image = self.IMGS[1]
+        elif self.image_count < self.ANIMATION_TIME * 3:
+            self.image = self.IMGS[2]
+        elif self.image_count < self.ANIMATION_TIME * 3:
+            self.image = self.IMGS[1]
+        elif self.image_count == self.ANIMATION_TIME * 4 + 1:
+            self.image = self.IMGS[0]
+            self.image_count = 0
+        
+        if self.tilt <= -80:
+            self.image = self.IMGS[1]
+            self.image_count = self.ANIMATION_TIME * 2
+            
+        rotated_image  = pygame.transform.rotate(self.image, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.image.get_rect(topleft=(self.x, self.y)).center)
+        
+        win.blit(rotated_image, new_rect.topleft)
+        
+    def get_mask(self):
+        return pygame.mask.from_surface(self.image)
+    
+def draw_window(win, bird):
+    win.blit(BG_IMG, (0,0))
+    bird.draw(win)
+    pygame.display.update()
+    
+def main():
+    bird = Bird(200, 200)
+    run = True
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        bird.move()
+        draw_window(win, bird)            
+    
+    pygame.quit()
+    quit()
+    
+main()
